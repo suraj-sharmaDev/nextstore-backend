893fb0220f935ea835d770cdf68b189a9fb236b3
suraj-sharmaDev <surajdroid97@gmail.com>
2020-07-29 19:25:55 +0530

    Updates

--- procedures/searchProcedures.sql
+++ procedures/searchProcedures.sql
@@ -1,9 +1,163 @@
---Search for products
-CREATE PROCEDURE dbo.spSearchProduct
-@searchKey NVARCHAR(100), @result NVARCHAR(max)
+---Search for products in shop with keyword
+CREATE PROCEDURE dbo.spSearchInShop
+@shopId INT, 
+@keyword NVARCHAR(100)
 AS
 BEGIN
+Declare @outputData NVARCHAR(MAX);
+DECLARE @query NVARCHAR(MAX);
+declare @tableId int;
 
-SELECT * from 
+If @shopId % 20 = 0
+	SET @tableId =  @shopId/20;
+ELSE
+	SET @tableId =  @shopId/20 + 1;
 
+declare @tableName varchar(100) = N'product'+ cast(@tableId as varchar);
+
+IF OBJECT_ID('tempdb..#Products') IS NOT NULL
+	Truncate TABLE #Products
+else
+	CREATE TABLE #Products 
+	(
+		id int,
+		name varchar(100),
+		image varchar(200),
+		subCategoryChildId int,
+		price int
+	 )
+	 
+IF OBJECT_ID('tempdb..#Categories') IS NOT NULL
+DROP TABLE #Categories;
+
+----all matching products added to temp table #Products
+Declare @search varchar(100) = '''%' + @keyword + '%''';
+SET @query = N'
+	select productMaster.id as id,
+	productMaster.name as name, 
+	productMaster.image as image, 
+	productMaster.subCategoryChildId as subCategoryChildId,
+	product.price as price
+	From
+		productMaster
+		INNER JOIN '+ @tableName + ' as product on product.productMasterId = productMaster.id
+	where productMaster.name LIKE ' + @search + '
+	and product.shopId = ' + cast(@shopId as varchar) + '
+	Order By productMaster.subCategoryChildId OFFSET 0 ROWS
+	';
+
+INSERT INTO #Products
+	EXEC sp_executesql @query;
+
+----all categories and subCategories for the products retrieved into #Categories
+
+select 
+subCategory.categoryId as categoryId,
+subCategory.id as subCategoryId,
+subCategoryChild.id as subCategoryChildId
+INTO #Categories
+from 
+subCategoryChild
+INNER JOIN subCategory on subCategory.id = subCategoryChild.subCategoryId 
+where subCategoryChild.id in (
+	select subCategoryChildId from #Products
+); 
+
+with x(json) as (
+	select 
+	categoryId = categoryId,
+	subCategoryId = subCategoryId,
+	subCategoryChildId = subCategoryChildId,
+	data = (
+		select id, name, image, price from #Products
+		where subCategoryChildId = C.subCategoryChildId
+		FOR JSON PATH, INCLUDE_NULL_VALUES 
+	)
+	From 
+	#Categories C 
+	FOR JSON PATH, INCLUDE_NULL_VALUES
+)
+
+ select @outputData=json from x;
+ select @outputData;
+ RETURN
 END
+
+GO;
+
+----------------------------------------------------------------
+
+--Get all shops having products with keyword
+--constraint to `near the user`
+
+CREATE PROCEDURE dbo.spSearchShopsWithProduct
+@custLat FLOAT, @custLng FLOAT, @keyword NVARCHAR(100)
+AS
+BEGIN
+	DECLARE @id INT;
+	DECLARE @name NVARCHAR(100);
+	DECLARE @category NVARCHAR(30);
+	DECLARE @image NVARCHAR(100);
+	DECLARE @onlineStatus bit;
+	
+	DECLARE @tableId int;
+	DECLARE @tableName varchar(100);
+	DECLARE @query NVARCHAR(MAX);
+	DECLARE @search varchar(100) = '''%' + @keyword + '%''';
+	--This procedure is combination of two separate procedures
+	-- 1st Find all shops near the user
+	-- 2nd find products in this shop matching keyword
+	-- For faster results lets not use second one
+	-- instead write out own sub query
+	
+	DECLARE @stagingTable table (
+		shopId INT,
+		name nvarchar(100),
+		category nvarchar(50),
+		onlineStatus bit,
+		image nvarchar(100),
+		distance DECIMAL(3, 3)
+	);
+	DECLARE @searchFlag INT; --to check if product exist in shop
+	INSERT Into @stagingTable exec spfindShopsNearby @custLat, @custLng;
+
+	Declare shopCursor cursor for
+		select shopId, name, category, onlineStatus, image from @stagingTable
+	Open shopCursor 
+	
+	Fetch next from shopCursor into @id, @name, @category, @onlineStatus, @image 
+	
+	While(@@FETCH_STATUS=0)
+	Begin
+		--each of the row this cursor points to is shop near this user
+		--For each loop set @searchFlag to null, to not hold past value
+		SET @searchFlag = NULL;
+		--find the table name for the shop with shopId = @id
+		If @id % 20 = 0
+			SET @tableId =  @id/20;
+		ELSE
+			SET @tableId =  @id/20 + 1;
+		
+		SET @tableName = N'product'+ cast(@tableId as varchar);
+	
+		SET @query = N'
+			select
+			TOP 1
+			@searchFlag = productMaster.id
+			From
+			productMaster
+			INNER JOIN product1 as product on product.productMasterId = productMaster.id
+			where (productMaster.name LIKE '+ @search +'
+				and product.shopId = ' + cast(@id as varchar) + ' )';
+			
+		EXEC sp_executesql @query, N'@searchFlag INT OUTPUT', @searchFlag=@searchFlag OUTPUT;
+		if(@searchFlag is NULL)
+		begin
+			Delete from @stagingTable where shopId=@id
+		end
+		Fetch next from shopCursor into @id, @name, @category, @onlineStatus, @image
+	END
+	SELECT * FROM @stagingTable;
+END
+
+GO;
---
+++ procedures/structure.sql
@@ -0,0 +1,217 @@
+-- nextstore.dbo.category definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.category GO
+
+CREATE TABLE nextstore.dbo.category (
+	id int IDENTITY(1,1) NOT NULL,
+	name nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK__category__3213E83F1A37568D PRIMARY KEY (id)
+) GO;
+
+
+-- nextstore.dbo.customer definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.customer GO
+
+CREATE TABLE nextstore.dbo.customer (
+	id int IDENTITY(1,1) NOT NULL,
+	name nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	mobile nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	fcmToken nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK__customer__3213E83FA6F76262 PRIMARY KEY (id)
+) GO;
+
+
+-- nextstore.dbo.merchant definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.merchant GO
+
+CREATE TABLE nextstore.dbo.merchant (
+	id int IDENTITY(1,1) NOT NULL,
+	firstName nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	lastName nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	mobile nvarchar(30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	createdAt datetimeoffset NOT NULL,
+	updatedAt datetimeoffset NOT NULL,
+	CONSTRAINT PK__merchant__3213E83F68F8C78B PRIMARY KEY (id)
+) GO;
+
+
+-- nextstore.dbo.orderMaster definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.orderMaster GO
+
+CREATE TABLE nextstore.dbo.orderMaster (
+	id int IDENTITY(1,1) NOT NULL,
+	customerId int NULL,
+	shopId int NULL,
+	status nvarchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	createdAt datetimeoffset NULL,
+	CONSTRAINT PK__orderMas__3213E83F7DB14EFF PRIMARY KEY (id)
+) GO;
+
+
+-- nextstore.dbo.cart definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.cart GO
+
+CREATE TABLE nextstore.dbo.cart (
+	id int IDENTITY(1,1) NOT NULL,
+	productId int NULL,
+	name nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[image] nvarchar(180) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	price int NULL,
+	qty int NULL,
+	customerId int NULL,
+	CONSTRAINT PK__cart__3213E83FB0A13225 PRIMARY KEY (id),
+	CONSTRAINT FK__cart__customerId__0D7A0286 FOREIGN KEY (customerId) REFERENCES nextstore.dbo.customer(id) ON DELETE SET NULL
+) GO;
+
+
+-- nextstore.dbo.customerAddress definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.customerAddress GO
+
+CREATE TABLE nextstore.dbo.customerAddress (
+	id int IDENTITY(1,1) NOT NULL,
+	label nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	addressName nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	landmark nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	latitude float NULL,
+	longitude float NULL,
+	reverseAddress nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	customerId int NULL,
+	CONSTRAINT PK__customer__3213E83FFEE64773 PRIMARY KEY (id),
+	CONSTRAINT FK__customerA__custo__0A9D95DB FOREIGN KEY (customerId) REFERENCES nextstore.dbo.customer(id) ON DELETE SET NULL
+) GO;
+
+
+-- nextstore.dbo.orderDetail definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.orderDetail GO
+
+CREATE TABLE nextstore.dbo.orderDetail (
+	id int IDENTITY(1,1) NOT NULL,
+	productId int NULL,
+	productName nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	qty int NULL,
+	price int NULL,
+	orderMasterId int NULL,
+	CONSTRAINT PK__orderDet__3213E83F4619239D PRIMARY KEY (id),
+	CONSTRAINT FK__orderDeta__order__15DA3E5D FOREIGN KEY (orderMasterId) REFERENCES nextstore.dbo.orderMaster(id)
+) GO;
+
+
+-- nextstore.dbo.shop definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.shop GO
+
+CREATE TABLE nextstore.dbo.shop (
+	id int IDENTITY(1,1) NOT NULL,
+	name nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	category nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	onlineStatus bit NULL,
+	fcmToken nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	coverage int NULL,
+	merchantId int NULL,
+	[image] nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK__shop__3213E83FAF52DF09 PRIMARY KEY (id),
+	CONSTRAINT FK__shop__merchantId__0B5CAFEA FOREIGN KEY (merchantId) REFERENCES nextstore.dbo.merchant(id) ON DELETE SET NULL
+) GO;
+
+
+-- nextstore.dbo.shopAddress definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.shopAddress GO
+
+CREATE TABLE nextstore.dbo.shopAddress (
+	id int IDENTITY(1,1) NOT NULL,
+	pickupAddress nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	latitude float NULL,
+	longitude float NULL,
+	shopId int NULL,
+	CONSTRAINT PK__shopAddr__3213E83F70869032 PRIMARY KEY (id),
+	CONSTRAINT FK__shopAddre__shopI__0E391C95 FOREIGN KEY (shopId) REFERENCES nextstore.dbo.shop(id)
+) GO;
+
+
+-- nextstore.dbo.subCategory definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.subCategory GO
+
+CREATE TABLE nextstore.dbo.subCategory (
+	id int IDENTITY(1,1) NOT NULL,
+	name nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	categoryId int NULL,
+	CONSTRAINT PK__subCateg__3213E83F7CAB3994 PRIMARY KEY (id),
+	CONSTRAINT FK__subCatego__categ__18B6AB08 FOREIGN KEY (categoryId) REFERENCES nextstore.dbo.category(id)
+) GO;
+
+
+-- nextstore.dbo.subCategoryChild definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.subCategoryChild GO
+
+CREATE TABLE nextstore.dbo.subCategoryChild (
+	id int IDENTITY(1,1) NOT NULL,
+	name nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	subCategoryId int NULL,
+	CONSTRAINT PK__subCateg__3213E83F94F8A0C7 PRIMARY KEY (id),
+	CONSTRAINT FK__subCatego__subCa__1B9317B3 FOREIGN KEY (subCategoryId) REFERENCES nextstore.dbo.subCategory(id)
+) GO;
+
+
+-- nextstore.dbo.productMaster definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.productMaster GO
+
+CREATE TABLE nextstore.dbo.productMaster (
+	id int IDENTITY(1,1) NOT NULL,
+	name nvarchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[image] nvarchar(180) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	subCategoryChildId int NULL,
+	CONSTRAINT PK__productM__3213E83FBAAA4E7D PRIMARY KEY (id),
+	CONSTRAINT FK__productMa__subCa__1E6F845E FOREIGN KEY (subCategoryChildId) REFERENCES nextstore.dbo.subCategoryChild(id)
+) GO;
+
+
+-- nextstore.dbo.product1 definition
+
+-- Drop table
+
+-- DROP TABLE nextstore.dbo.product1 GO
+
+CREATE TABLE nextstore.dbo.product1 (
+	id int IDENTITY(1,1) NOT NULL,
+	mrp int NULL,
+	price int NULL,
+	shopId int NULL,
+	productMasterId int NULL,
+	CONSTRAINT PK__product__3213E83F16B61479 PRIMARY KEY (id),
+	CONSTRAINT FK__product__product__22401542 FOREIGN KEY (productMasterId) REFERENCES nextstore.dbo.productMaster(id),
+	CONSTRAINT FK__product__shopId__214BF109 FOREIGN KEY (shopId) REFERENCES nextstore.dbo.shop(id)
+) GO;
