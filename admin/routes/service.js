9bd66ad7fcb42463ec95984f63b0a585e7d69c24
suraj-sharmaDev <surajdroid97@gmail.com>
2020-09-27 12:30:49 +0530

    Added service admin apis

--- admin/routes/service.js
+++ admin/routes/service.js
@@ -2,8 +2,33 @@ const express = require('express');
 const {sequelize} = require('../models');
 const router = express.Router();
 
-router.get('/', (req, res, next)=>{
-    res.send(__dirname+'../../assets');
+router.get('/:page?', async(req, res, next) => {
+    let error = true;
+    let status = null;
+    let page = req.params.page ? req.params.page : 1;
+    try {
+        status = await sequelize.query(
+            'exec spGetServiceProvidersWithFilter :json, :page',
+            {
+                replacements: {
+                    json: JSON.stringify(req.query),
+                    page: page
+                }
+            }).spread((value, message) => {
+                //since the return data will be string and not parsed
+                //lets parse it
+                return value;
+            });
+        error = false;
+    } catch (err) {
+        error = true;
+        console.log(err);
+    }
+    if(error){
+        res.send({error: true, message: 'db_error'});
+    }else{
+        res.send(status);
+    }
 })
 
 module.exports = router;
