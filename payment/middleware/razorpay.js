9be5e834d0c2ccdbc023e5741f3dd33decf6dca3
unknown <suraj.sharma@emvigotech.com>
2021-09-12 17:44:49 +0530

    Test key added for server

--- payment/middleware/razorpay.js
+++ payment/middleware/razorpay.js
@@ -1,16 +1,16 @@
 const Razorpay = require('razorpay');
 
 /** TEST */
-// const instance = new Razorpay({
-//     key_id: 'rzp_test_3pbm7wyjs8ArmH', 
-//     key_secret: 'bNM8MZEMuC6OxatJ4dlKAx04'
-// })
+const instance = new Razorpay({
+    key_id: 'rzp_test_mbxyyMiUsc7oYz', 
+    key_secret: 'Z4Bfn0BM28UzTjI5EkJ2dJTL'
+})
 
 /** PRODUCTION */
-const instance = new Razorpay({
-    key_id: 'rzp_live_kixdopwwSaWHLu', 
-    key_secret: 'QnBPOAu39SIaXTj2Ne1d7MKA'
-})
+// const instance = new Razorpay({
+//     key_id: 'rzp_live_kixdopwwSaWHLu', 
+//     key_secret: 'QnBPOAu39SIaXTj2Ne1d7MKA'
+// })
 
 const createOrder = (options) => {
     return new Promise((resolve, reject)=> {
